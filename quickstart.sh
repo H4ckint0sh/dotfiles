a3576fa7ac79551133943673240c10f73d7756a5
H4ckint0sh <44159647+H4ckint0sh@users.noreply.github.com>
2023-03-06 22:25:08 +0100

    Update quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -14,4 +14,24 @@ source ./linkfolders
 echo "installing files and folders ..."
 source ./install
 
-echo "Done."
+echo "Restarting affected apps"
+for app in "Activity Monitor" \
+    "Address Book" \
+    "Calendar" \
+    "cfprefsd" \
+    "Contacts" \
+    "Dock" \
+    "Finder" \
+    "Google Chrome Canary" \
+    "Google Chrome" \
+    "Mail" \
+    "Messages" \
+    "Opera" \
+    "Photos" \
+    "Safari" \
+    "SystemUIServer" \
+    "Terminal" \
+    "Transmission"; do
+    killall "${app}" &> /dev/null
+done
+echo "Done. Note that some of these changes require a logout/restart to take effect."
