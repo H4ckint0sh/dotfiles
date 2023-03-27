566e2954492ccd72648445192cf64def1d63e557
Alijan Adeli <alijan.adeli@consid.se>
2023-03-27 21:43:05 +0200

    C-t -> toggle

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -7,15 +7,8 @@ require("toggleterm").setup {
       return vim.o.columns * 0.4
     end
   end,
-    open_mapping = [[<leader>tr]],
+    open_mapping = [[<C-t>]],
   ---@diagnostic disable-next-line: unused-local
-  on_open = function(term)
-    require('shade').toggle();
-  end,
-  ---@diagnostic disable-next-line: unused-local
-  on_close = function(term)
-    require('shade').toggle();
-  end,
   highlights = {
     -- highlights which map to a highlight group name and a table of it's values
     -- NOTE: this is only a subset of values, any group placed here will be set for the terminal window split
