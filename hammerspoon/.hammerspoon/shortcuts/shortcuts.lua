692d0962cb919181c58689c2dff04d1c77e90f76
H4ckint0sh <alijan.adeli@icloud.com>
2023-05-26 16:10:57 +0200

    adding kitty

--- hammerspoon/.hammerspoon/shortcuts/shortcuts.lua
+++ hammerspoon/.hammerspoon/shortcuts/shortcuts.lua
@@ -13,13 +13,13 @@ end
 -- Hammerspoon will try to open applications with same order passed in as argument
 -- If Microsoft Teams is opened then Spotify app will not open
 -- and if Microsoft Teams is not found then Spotify will be opened so on and so forth
-hs.hotkey.bind({ "alt", "shift" }, "W", open_app({ "WezTerm" }))
+hs.hotkey.bind({ "alt", "shift" }, "T", open_app({ "kitty" }))
 hs.hotkey.bind({ "alt", "shift" }, "S", open_app({ "Spotify" }))
 -- hs.hotkey.bind({ "cmd", "shift" }, "D", open_app({ "Discord" }))
 -- hs.hotkey.bind({ "cmd", "shift" }, "W", open_app({ "IntelliJ IDEA", "Webstorm" }))
 -- hs.hotkey.bind({ "cmd", "shift" }, "P", open_app({ "Postman" }))
 
-hs.hotkey.bind({ "alt", "shift" }, "T", open_app({ "Microsoft Teams" }))
-hs.hotkey.bind({ "alt", "shift" }, "O", open_app({ "Microsoft Outlook" }))
+-- hs.hotkey.bind({ "alt", "shift" }, "T", open_app({ "Microsoft Teams" }))
+-- hs.hotkey.bind({ "alt", "shift" }, "O", open_app({ "Microsoft Outlook" }))
 hs.hotkey.bind({ "alt", "shift" }, "F", open_app({ "Firefox" }))
 hs.hotkey.bind({ "alt", "shift" }, "V", open_app({ "Visual Studio Code" }))
