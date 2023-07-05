b127dfcd4d3841d780136514514780486f6bb2ad
H4ckint0sh <alijan.adeli@icloud.com>
2023-07-05 09:07:47 +0200

    functions for setup script

---
+++ functions.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+running_macos() {
+  [ "$(uname)" == Darwin ]
+  return $?
+}
+
+command_available() {
+  which "$1" >/dev/null 2>&1
+}
+
+fzf_available() {
+  command_available fzf
+}
+
+fish_available() {
+  command_available fish
+}
+
+brew_available() {
+  command_available brew
+}
+
+load_brew_shellenv() {
+  if test -x /opt/homebrew/bin/brew; then
+    brew=/opt/homebrew/bin/brew
+  elif test -x /usr/local/bin/brew; then
+    brew=/usr/local/bin/brew
+  fi
+
+  if test -n "${brew}"; then
+    eval "$($brew shellenv)"
+  fi
+}
