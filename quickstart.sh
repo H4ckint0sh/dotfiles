e4bf386f97c8c927a40e0a94b4b0f1b3f7a42807
H4ckint0sh <alijan.adeli@icloud.com>
2023-07-05 09:08:44 +0200

    prevent sleep - install brew & fish

--- quickstart.sh
+++ quickstart.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
 
-#set -e
+set -e
+
+# shellcheck source=./functions.sh
+source ./functions.sh
+
+if running_macos; then
+  # Prevent sleeping during script execution, as long as the machine is on AC power
+  caffeinate -s -w $$ &
+fi
 
 echo "Making Files ..."
 make -f Makefile
 
+if ! brew_available; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  load_brew_shellenv
+fi
+
 echo "Brewing ..."
 source ./macsetup
+
+if fish_available; then
+  ./fish.sh
+fi
+
+if running_macos; then
+  load_brew_shellenv
+
+fi
 
 echo "linking your files ..."
 source ./linkfolders
@@ -14,16 +36,12 @@ source ./linkfolders
 echo "installing files and folders ..."
 source ./install
 
-# fisher https://github.com/jorgebucaran/fisher
-curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-fisher install FabioAntunes/fish-nvm edc/bass franciscolourenco/done
-
 echo "Installing nvm"
 mkdir ~/.nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 nvm install 'lts/*'
 
-echo "Installing 
+echo "Installing tmux plugins"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ~/.tmux/plugins/tpm/bin/install_plugins
 
@@ -50,4 +68,5 @@ for app in "Activity Monitor" \
     "Transmission"; do
     killall "${app}" &> /dev/null
 done
+
 echo "Done. Note that some of these changes require a logout/restart to take effect."
