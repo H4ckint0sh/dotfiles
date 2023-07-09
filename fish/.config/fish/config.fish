ab9d331f57d39ac2b4f88023818f4412e46223ee
H4ckint0sh <alijan.adeli@icloud.com>
2023-07-09 21:09:52 +0200

    adding homebrew sbin

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -19,6 +19,7 @@ set -Ux VISUAL hx
 # ordered by priority - bottom up
 fish_add_path /usr/local/bin
 fish_add_path /opt/homebrew/bin
+fish_add_path /opt/homebrew/sbin
 fish_add_path /usr/local/sbin
 fish_add_path $HOME/.cargo/bin
 fish_add_path $PNPM_HOME
