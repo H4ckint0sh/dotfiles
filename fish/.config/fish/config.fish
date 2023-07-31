a1f0c0b04dce0ef69a33e1152e160592f3fd3547
H4ckint0sh <alijan.adeli@icloud.com>
2023-07-31 14:37:06 +0200

    adding helix runtime and changing node path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,14 +6,15 @@ zoxide init fish | source # 'ajeetdsouza/zoxide'
 thefuck --alias | source # thefuck 
 
 # set -gx PNPM_HOME /Users/ali/Library/pnpm # https://pnpm.io/
-set -gx DOTFILES /Users/ali/.dotfiles 
+set -Ux DOTFILES /Users/ali/.dotfiles 
+set -Ux HELIX_RUNTIME /Users/ali/Desktop/projects/helix/runtime 
 set -Ux BAT_THEME Nord # 'sharkdp/bat' cat clone 
 set -Ux EDITOR hx # 'neovim/neovim' text editor 
 set -Ux fish_greeting # disable fish greeting
-set -U  FZF_DEFAULT_COMMAND "fd -H -E '.git'"
+set -Ux FZF_DEFAULT_COMMAND "fd -H -E '.git'"
 set -Ux LANG en_US.UTF-8
 set -Ux LC_ALL en_US.UTF-8
-set -Ux NODE_PATH "~/.nvm/versions/node/v18.16.1/bin/node" # 'nvm-sh/nvm'
+set -Ux NODE_PATH ~/.local/share/nvm/v18.16.1/bin/node
 set -Ux VISUAL hx
 
 # ordered by priority - bottom up
@@ -28,3 +29,5 @@ fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.tmux/plugins/t-smart-tmux-session-manager/bin
 fish_add_path $HOME/bin # my custom scripts
 fish_add_path $HOME/.nvm/versions/node/v18.16.1/bin
+fish_add_path $HOME/.local/share/nvm/v18.16.1/bin
+
