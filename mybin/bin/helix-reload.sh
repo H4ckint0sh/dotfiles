294583abe79fb633f74f4cffcc5d42fdb19ed502
H4ckint0sh <44159647+H4ckint0sh@users.noreply.github.com>
2023-08-30 12:11:06 +0200

    fixed comments

--- mybin/bin/helix-reload.sh
+++ mybin/bin/helix-reload.sh
@@ -13,7 +13,7 @@ if [ -n "$helix_pane_index" ]; then
   # Send ":" to start command input in Helix
   tmux send-keys ":"
 
-  # Send the "open" command with file path(s) to the pane and simulate Enter
+  # Send the "reload" command to the pane and simulate Enter
   tmux send-keys "reload-all" C-m
 
 else  
