839968c8be63ebdda169a2ed855bcf9d24438a3d
H4ckint0sh <alijan.adeli@icloud.com>
2023-08-31 08:38:14 +0200

    fixing typo in comments

--- bin/bin/helix-reload.sh
+++ bin/bin/helix-reload.sh
@@ -6,7 +6,6 @@ LIST_DATA="#{pane_index} #{pane_current_command}"
 # Find pane id where helix is running
 HELIX_PANE_INDEX=$(tmux list-panes -a -F "$LIST_DATA" | grep "hx" | awk '{print $1}')
 
-# Send reload command if such a pane exists
 if [ -n "$HELIX_PANE_INDEX" ]; then
 
   # change to helix pane
@@ -15,7 +14,7 @@ if [ -n "$HELIX_PANE_INDEX" ]; then
   # Send ":" to start command input in Helix
   tmux send-keys ":"
 
-  # Send the "open" command with file path(s) to the pane and simulate Enter
+  # Send the "reload-all" command to helix pane and simulate Enter
   tmux send-keys "reload-all" C-m
 
 fi
