de100acebbbe58f2e08598da6dee4673259e52ac
H4ckint0sh <alijan.adeli@icloud.com>
2023-09-26 01:28:49 +0200

    open file picker upon ide start

--- bin/bin/start-ide.sh
+++ bin/bin/start-ide.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-SESSION="IDE"
 
 INITDIR=$(pwd)
 WORKDIR=$(dirname "$0")
 cd "$WORKDIR"
+
+SESSION=$(basename "$INITDIR")
 
 tmux \
 	new-session -s "$SESSION" \; \
 	send-keys 'nnn -c '"$INITDIR"' -f -o -e' Enter \; \
 	split-window -c "$INITDIR" -h -p 85 hx \; \
 	send-keys 'hx' Enter \; \
+	send-keys " " "f" \; \
 	new-window -c "$INITDIR" -d \; \
