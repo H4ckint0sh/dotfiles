52936e3cd63824837893a89c598680496ed8539b
H4ckint0sh <alijan.adeli@icloud.com>
2024-01-19 10:59:52 +0100

    fixing hyper

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -20,7 +20,98 @@ local shifopt = { "⌥", "⇧" }
 
 -- F19 and Hyperkey Hack
 -- Keys you want to use with hyper key go here:
-local hyperBindings = { 't', 'o', 'c', 'f', 'e', 'v', 'c', 'b', 'p', '\\', 'j', 'k', 'h', 'l', 'q', 'TAB' }
+local hyperBindings = { "a",
+	"b",
+	"c",
+	"d",
+	"e",
+	"f",
+	"g",
+	"h",
+	"i",
+	"j",
+	"k",
+	"l",
+	"m",
+	"n",
+	"o",
+	"p",
+	"q",
+	"r",
+	"s",
+	"t",
+	"u",
+	"v",
+	"w",
+	"x",
+	"y",
+	"z",
+	"0",
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9",
+	"`",
+	"=",
+	"-",
+	"]",
+	"[",
+	"\'",
+	";",
+	"\\",
+	",",
+	"/",
+	".",
+	"§",
+	"f1",
+	"f2",
+	"f3",
+	"f4",
+	"f5",
+	"f6",
+	"f7",
+	"f8",
+	"f9",
+	"f10",
+	"f11",
+	"f12",
+	"pad.",
+	"pad*",
+	"pad+",
+	"pad/",
+	"pad-",
+	"pad=",
+	"pad0",
+	"pad1",
+	"pad2",
+	"pad3",
+	"pad4",
+	"pad5",
+	"pad6",
+	"pad7",
+	"pad8",
+	"pad9",
+	"padclear",
+	"padenter",
+	"return",
+	"tab",
+	"space",
+	"delete",
+	"help",
+	"home",
+	"pageup",
+	"forwarddelete",
+	"end",
+	"pagedown",
+	"left",
+	"right",
+	"down",
+	"up" }
 
 local k = hs.hotkey.modal.new({}, "F17")
 
@@ -68,3 +159,61 @@ hs.hotkey.bind(shifopt, 'c', function() toggle_app('Microsoft Teams (work or sch
 hs.hotkey.bind(shifopt, 'o', function() toggle_app('Microsoft Outlook') end)
 hs.hotkey.bind(shifopt, 't', function() toggle_app('Wezterm') end)
 hs.hotkey.bind(shifopt, 'f', function() toggle_app('Finder') end)
+
+-- caffeinate Icons
+local ampOnIcon = [[ASCII:
+.....1a..........AC..........E
+..............................
+......4.......................
+1..........aA..........CE.....
+e.2......4.3...........h......
+..............................
+..............................
+.......................h......
+e.2......6.3..........t..q....
+5..........c..........s.......
+......6..................q....
+......................s..t....
+.....5c.......................
+]]
+
+local ampOffIcon = [[ASCII:
+.....1a.....x....AC.y.......zE
+..............................
+......4.......................
+1..........aA..........CE.....
+e.2......4.3...........h......
+..............................
+..............................
+.......................h......
+e.2......6.3..........t..q....
+5..........c..........s.......
+......6..................q....
+......................s..t....
+...x.5c....y.......z..........
+]]
+
+-- caffeine replacement
+local caffeine = hs.menubar.new()
+
+local function setCaffeineDisplay(state)
+	if state then
+		caffeine:setIcon(ampOnIcon)
+	else
+		caffeine:setIcon(ampOffIcon)
+	end
+end
+
+local function caffeineClicked()
+	setCaffeineDisplay(hs.caffeinate.toggle("displayIdle"))
+end
+
+if caffeine then
+	caffeine:setClickCallback(caffeineClicked)
+	setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
+end
+
+-- Spotify shortcuts
+hs.hotkey.bind(shifopt, 'p', hs.spotify.playpause)
+hs.hotkey.bind(f18, 'right', hs.spotify.next)
+hs.hotkey.bind(f18, 'left', hs.spotify.previous)
