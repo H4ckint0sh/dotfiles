1a89c7044cacfd027bbca26c199922f10cc66109
H4ckint0sh <alijan.adeli@icloud.com>
2024-06-05 11:34:28 +0200

    change browser to Firefox

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -81,7 +81,8 @@ local toggleApp = function(appName, launch)
 end
 
 hs.hotkey.bind(shifopt, "T", function() toggleApp("Alacritty", true) end)
-hs.hotkey.bind(shifopt, "B", function() toggleApp("Chromium", true) end)
+hs.hotkey.bind(shifopt, "B", function() toggleApp("Firefox", true) end)
+hs.hotkey.bind(shifopt, "G", function() toggleApp("Google Chrome", true) end)
 hs.hotkey.bind(shifopt, "S", function() toggleApp("spotify", true) end)
 hs.hotkey.bind(shifopt, "C", function() toggleApp("Microsoft Teams (work or school)", true) end)
 hs.hotkey.bind(shifopt, "M", function() toggleApp("Microsoft Outlook", true) end)
