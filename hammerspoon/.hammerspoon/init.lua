f98dbdd8d75a3f09f5d8115bf6336a80fdbc37bb
H4ckint0sh <alijan.adeli@icloud.com>
2024-06-29 00:27:01 +0200

    wezterm

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -80,7 +80,7 @@ local toggleApp = function(appName, launch)
 	end
 end
 
-hs.hotkey.bind(shifopt, "T", function() toggleApp("kitty", true) end)
+hs.hotkey.bind(shifopt, "T", function() toggleApp("wezterm", true) end)
 hs.hotkey.bind(shifopt, "B", function() toggleApp("Firefox", true) end)
 hs.hotkey.bind(shifopt, "G", function() toggleApp("Google Chrome", true) end)
 hs.hotkey.bind(shifopt, "S", function() toggleApp("spotify", true) end)
