6e823441ae9ea1200aa635b4a47026497d7db318
H4ckint0sh <alijan.adeli@icloud.com>
2024-07-19 12:11:20 +0200

    no search in bottom

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -11,7 +11,7 @@ return {
 	},
 
 	opts = {
-
+		messages = { enabled = true },
 		lsp = {
 			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 			override = {
@@ -22,11 +22,17 @@ return {
 		},
 		-- you can enable a preset for easier configuration
 		presets = {
-			bottom_search = true, -- use a classic bottom cmdline for search
+			bottom_search = false, -- use a classic bottom cmdline for search
 			command_palette = true, -- position the cmdline and popupmenu together
 			long_message_to_split = true, -- long messages will be sent to a split
 			inc_rename = false, -- enables an input dialog for inc-rename.nvim
 			lsp_doc_border = false, -- add a border to hover docs and signature help
+		},
+		routes = {
+			{
+				view = "notify",
+				filter = { event = "msg_showmode" },
+			},
 		},
 	},
 
