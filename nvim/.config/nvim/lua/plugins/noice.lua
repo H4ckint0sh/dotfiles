a84b64c3ca86ab1546b8be71445671b7cecf77ec
H4ckint0sh <alijan.adeli@icloud.com>
2024-08-12 08:19:49 +0200

    dismiss notifications binding

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -26,16 +26,30 @@ return {
 			command_palette = true, -- position the cmdline and popupmenu together
 			long_message_to_split = true, -- long messages will be sent to a split
 			inc_rename = false, -- enables an input dialog for inc-rename.nvim
-			lsp_doc_border = false, -- add a border to hover docs and signature help
+			lsp_doc_border = true, -- add a border to hover docs and signature help
 		},
 		routes = {
 			{
 				view = "notify",
 				filter = { event = "msg_showmode" },
 			},
+			-- https://github.com/LazyVim/LazyVim/discussions/830
+			{
+				filter = {
+					event = "notify",
+					find = "No information available",
+				},
+				opts = { skip = true },
+			},
 		},
 	},
-
+	keys = {
+		{
+			"<leader>nd",
+			vim.cmd.NoiceDismiss,
+			desc = "Dismiss notification",
+		},
+	},
 	config = function(_, opts)
 		require("noice").setup(opts)
 	end,
