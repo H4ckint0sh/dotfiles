3d4d6b5618a734ebc6dfda7046dcdd3dfca8d667
H4ckint0sh <alijan.adeli@icloud.com>
2024-08-30 07:50:52 +0200

    change binding to G

--- nvim/.config/nvim/lua/plugins/lazygit.lua
+++ nvim/.config/nvim/lua/plugins/lazygit.lua
@@ -14,6 +14,6 @@ return {
 	-- setting the keybinding for LazyGit with 'keys' is recommended in
 	-- order to load the plugin when the command is run for the first time
 	keys = {
-		{ "<leader>g", "<cmd>LazyGit<cr>", { silent = true }, desc = "LazyGit" },
+		{ "<leader>G", "<cmd>LazyGit<cr>", { silent = true }, desc = "LazyGit" },
 	},
 }
