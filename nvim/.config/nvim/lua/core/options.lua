5efb1bf21bbfd957d9f1844a335a8d7da80921e2
H4ckint0sh <alijan.adeli@icloud.com>
2024-09-07 00:44:41 +0200

    show changing text in a split

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -42,6 +42,7 @@ local options = {
 	fileencoding = "utf-8", --- The encoding written to file
 	incsearch = true, --- Start searching before pressing enter
 	showmode = false, --- Don't show things like -- INSERT -- anymore
+	inccommand = "split",
 }
 
 vim.opt.shortmess:append("c")
