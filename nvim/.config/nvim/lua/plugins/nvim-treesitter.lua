dff7fdcf51753b47a20dbeeb19c31f904e35d494
H4ckint0sh <alijan.adeli@icloud.com>
2024-09-09 21:29:02 +0200

    add styled

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -36,6 +36,7 @@ return {
 				"astro",
 				"vimdoc",
 				"astro",
+				"styled",
 			}, -- one of "all", or a list of languages
 			sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
 			ignore_install = { "haskell" }, -- list of parsers to ignore installing
