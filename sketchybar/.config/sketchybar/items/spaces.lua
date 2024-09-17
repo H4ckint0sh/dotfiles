62420c28703ed2a2da65feb8ffc19c79c8a601df
H4ckint0sh <alijan.adeli@icloud.com>
2024-09-18 00:53:20 +0200

    fixing flickering

--- sketchybar/.config/sketchybar/items/spaces.lua
+++ sketchybar/.config/sketchybar/items/spaces.lua
@@ -12,20 +12,29 @@ local LIST_CURRENT = "aerospace list-workspaces --focused"
 local spaces = {}
 local workspaceToMonitorMap = {}
 
--- Function to get the app icon
+-- Cache para íconos de aplicaciones para evitar búsquedas repetitivas
+local icon_cache = {}
+
+-- Function to get the icon for an application
 local function getIconForApp(appName)
-	return app_icons[appName] or "?"
+	if icon_cache[appName] then
+		return icon_cache[appName]
+	end
+	local icon = app_icons[appName] or app_icons["default"] or "?"
+	icon_cache[appName] = icon
+	return icon
+	--return app_icons[appName] or app_icons["default"] or "?"
 end
 
 -- Function to update workspace icons
 local function updateSpaceIcons(spaceId, workspaceName)
-	local icon_strip = ""
-	local shouldDraw = false
-
 	sbar.exec(LIST_APPS:format(workspaceName), function(appsOutput)
 		if not appsOutput then
 			return
 		end
+
+		local icon_strip = ""
+		local shouldDraw = false
 
 		for app in appsOutput:gmatch("[^\r\n]+") do
 			local appName = app:match("^%s*(.-)%s*$")
@@ -35,19 +44,17 @@ local function updateSpaceIcons(spaceId, workspaceName)
 			end
 		end
 
-		-- Verify if the item exists before trying to update it
 		if spaces[spaceId] then
 			spaces[spaceId].item:set({
 				label = { string = icon_strip, drawing = shouldDraw },
 			})
 		else
-			print("Warning: Workspace '" .. spaceId .. "' not found to update icons.")
 		end
 	end)
 end
 
--- Function to add a workspace item
-local function addWorkspaceItem(workspaceName, monitorId, isSelected)
+-- Function to add or update a workspace item
+local function addOrUpdateWorkspaceItem(workspaceName, monitorId, isSelected)
 	local spaceId = "workspace_" .. workspaceName .. "_" .. monitorId
 
 	if not spaces[spaceId] then
@@ -121,13 +128,11 @@ local function removeWorkspaceItem(spaceId)
 		if workspaceName then
 			workspaceToMonitorMap[workspaceName] = nil
 		end
+	else
 	end
 end
 
-local cachedMonitors = {}
-local cachedWorkspaces = {}
-
--- Error Handling and Safe Execution
+-- Safe execution function
 local function safeExec(command, callback)
 	sbar.exec(command, function(output)
 		if output then
@@ -136,18 +141,18 @@ local function safeExec(command, callback)
 	end)
 end
 
--- Function to Get the List of Monitors
+-- Function to get the list of monitors
 local function getMonitors(callback)
 	safeExec(LIST_MONITORS, function(monitorsOutput)
-		local monitorList = {}
-		for monitorId in monitorsOutput:gmatch("[^\r\n]+") do
-			table.insert(monitorList, monitorId)
+		local monitors = {}
+		for monitor in monitorsOutput:gmatch("[^\r\n]+") do
+			table.insert(monitors, monitor)
 		end
-		callback(monitorList)
+		callback(monitors)
 	end)
 end
 
--- Function to Get the Currently Focused Workspace
+-- Function to get the focused workspace
 local function getFocusedWorkspace(callback)
 	safeExec(LIST_CURRENT, function(focusedWorkspaceOutput)
 		local focusedWorkspace = focusedWorkspaceOutput:match("[^\r\n]+")
@@ -155,7 +160,7 @@ local function getFocusedWorkspace(callback)
 	end)
 end
 
--- Function to Get and Sort Workspaces by Monitor
+-- Function to get and sort workspaces by monitor
 local function getWorkspacesForMonitor(monitorId, callback)
 	safeExec(LIST_WORKSPACES:format(monitorId), function(workspacesOutput)
 		local workspaces = {}
@@ -169,95 +174,46 @@ local function getWorkspacesForMonitor(monitorId, callback)
 	end)
 end
 
--- Function to Draw the Workspaces
-local function drawWorkspaces(monitorList, focusedWorkspace)
-	local updatedSpaces = {}
-
-	for _, monitorId in ipairs(monitorList) do
-		getWorkspacesForMonitor(monitorId, function(workspaces)
-			cachedWorkspaces[monitorId] = workspaces
-
-			for _, workspaceName in ipairs(workspaces) do
-				local spaceId = "workspace_" .. workspaceName .. "_" .. monitorId
-				local isSelected = workspaceName == focusedWorkspace
-				addWorkspaceItem(workspaceName, monitorId, isSelected)
-				updatedSpaces[spaceId] = true
-			end
-		end)
-	end
-
-	-- Remove obsolete workspaces
-	for spaceId, _ in pairs(spaces) do
-		if not updatedSpaces[spaceId] then
-			removeWorkspaceItem(spaceId)
-		end
-	end
-end
-
--- Cache System to Avoid Redundant Executions
-local function isCacheValid(monitorList, focusedWorkspace)
-	if #monitorList ~= #cachedMonitors then
-		return false
-	end
-
-	for i, monitorId in ipairs(monitorList) do
-		if monitorId ~= cachedMonitors[i] then
-			return false
-		end
-		if cachedWorkspaces[monitorId] == nil then
-			return false
-		end
-	end
-
-	return focusedWorkspace == cachedWorkspaces.focusedWorkspace
-end
-
--- Main Function to Draw the Spaces
-local function drawSpaces()
+-- Function to update all workspaces
+local function updateAllWorkspaces()
 	getMonitors(function(monitorList)
 		getFocusedWorkspace(function(focusedWorkspace)
-			if not isCacheValid(monitorList, focusedWorkspace) then
-				drawWorkspaces(monitorList, focusedWorkspace)
-				cachedMonitors = monitorList
-				cachedWorkspaces.focusedWorkspace = focusedWorkspace
+			local updatedSpaces = {} -- Guarda todos los worskpaces independiente de los monitores
+			for _, monitorId in ipairs(monitorList) do
+				getWorkspacesForMonitor(monitorId, function(workspaces)
+					for _, workspaceName in ipairs(workspaces) do
+						local spaceId = "workspace_" .. workspaceName .. "_" .. monitorId
+						local isSelected = workspaceName == focusedWorkspace
+						addOrUpdateWorkspaceItem(workspaceName, monitorId, isSelected)
+						updatedSpaces[spaceId] = true
+					end
+
+					-- Remove obsolete workspaces for this monitor
+					for spaceId in pairs(spaces) do
+						if not updatedSpaces[spaceId] and spaceId:match("_%d+$") == "_" .. monitorId then
+							removeWorkspaceItem(spaceId)
+						end
+					end
+				end)
 			end
 		end)
 	end)
 end
 
-drawSpaces()
+-- Initial setup
+updateAllWorkspaces()
 
 local space_window_observer = sbar.add("item", {
 	drawing = false,
 	updates = true,
 })
 
-space_window_observer:subscribe("aerospace_workspace_change", function()
-	drawSpaces()
-end)
-
-space_window_observer:subscribe("front_app_switched", function()
-	drawSpaces()
-end)
-
-space_window_observer:subscribe("space_windows_change", function()
-	sbar.exec(LIST_CURRENT, function(focusedWorkspaceOutput)
-		local focusedWorkspace = focusedWorkspaceOutput:match("[^\r\n]+")
-		if focusedWorkspace then
-			local monitorId = workspaceToMonitorMap[focusedWorkspace]
-			if monitorId then
-				-- print("Monitor: " .. monitorId)
-				local spaceId = "workspace_" .. focusedWorkspace .. "_" .. monitorId
-				-- print("id space: " .. spaceId)
-				updateSpaceIcons(spaceId, focusedWorkspace)
-				-- else
-				-- 	print("Warning: MonitorId for workspace '" .. focusedWorkspace .. "' not found")
-			end
-		end
-		-- Also update all workspaces if necessary
-		drawSpaces()
-	end)
-end)
+space_window_observer:subscribe(
+	{ "aerospace_workspace_change", "front_app_switched", "space_windows_change" },
+	function()
+		updateAllWorkspaces()
+	end
+)
 
 -- Indicator for swapping menus and spaces
 local spaces_indicator = sbar.add("item", {
@@ -282,39 +238,36 @@ local spaces_indicator = sbar.add("item", {
 	},
 })
 
-spaces_indicator:subscribe("swap_menus_and_spaces", function(env)
-	local currently_on = spaces_indicator:query().icon.value == icons.switch.on
+local function toggleSpacesIndicator(currently_on)
 	spaces_indicator:set({
 		icon = currently_on and icons.switch.off or icons.switch.on,
 	})
+end
+
+spaces_indicator:subscribe("swap_menus_and_spaces", function()
+	local currently_on = spaces_indicator:query().icon.value == icons.switch.on
+	toggleSpacesIndicator(currently_on)
 end)
 
-spaces_indicator:subscribe("mouse.entered", function(_)
+local function animateSpacesIndicator(entered)
 	sbar.animate("tanh", 30, function()
 		spaces_indicator:set({
 			background = {
-				color = { alpha = 1.0 },
-				border_color = { alpha = 0.5 },
+				color = { alpha = entered and 1.0 or 0.0 },
+				border_color = { alpha = entered and 0.5 or 0.0 },
 			},
-			icon = { color = colors.bg1 },
-			label = { width = "dynamic" },
+			icon = { color = entered and colors.bg1 or colors.grey },
+			label = { width = entered and "dynamic" or 0 },
 		})
 	end)
-end)
+end
 
-spaces_indicator:subscribe("mouse.exited", function(_)
-	sbar.animate("tanh", 30, function()
-		spaces_indicator:set({
-			background = {
-				color = { alpha = 0.0 },
-				border_color = { alpha = 0.0 },
-			},
-			icon = { color = colors.grey },
-			label = { width = 0 },
-		})
-	end)
+spaces_indicator:subscribe("mouse.entered", function()
+	animateSpacesIndicator(true)
 end)
-
-spaces_indicator:subscribe("mouse.clicked", function(_)
+spaces_indicator:subscribe("mouse.exited", function()
+	animateSpacesIndicator(false)
+end)
+spaces_indicator:subscribe("mouse.clicked", function()
 	sbar.trigger("swap_menus_and_spaces")
 end)
