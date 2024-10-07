9c4c68aa8dec64ecb44bf92966640a3a273b1f3c
H4ckint0sh <alijan.adeli@icloud.com>
2024-10-07 20:46:34 +0200

    update gitsigns config

--- nvim/.config/nvim/lua/plugins/git-signs.lua
+++ nvim/.config/nvim/lua/plugins/git-signs.lua
@@ -54,7 +54,7 @@ return {
 				ignore_whitespace_change_at_eol = true,
 			},
 			on_attach = function(bufnr)
-				local gs = package.loaded.gitsigns
+				local gitsigns = require("gitsigns")
 
 				local function map(mode, l, r, opts)
 					opts = opts or {}
@@ -65,28 +65,45 @@ return {
 				-- Navigation
 				map("n", "]c", function()
 					if vim.wo.diff then
-						return "]c"
+						vim.cmd.normal({ "]c", bang = true })
+					else
+						gitsigns.nav_hunk("next")
 					end
-					vim.schedule(function()
-						gs.next_hunk()
-					end)
-					return "<Ignore>"
-				end, { expr = true })
+				end)
 
 				map("n", "[c", function()
 					if vim.wo.diff then
-						return "[c"
+						vim.cmd.normal({ "[c", bang = true })
+					else
+						gitsigns.nav_hunk("prev")
 					end
-					vim.schedule(function()
-						gs.prev_hunk()
-					end)
-					return "<Ignore>"
-				end, { expr = true })
+				end)
 
 				-- Actions
-				map("n", "<leader>gb", function()
-					gs.blame_line({ full = true })
+				map("n", "<leader>hs", gitsigns.stage_hunk)
+				map("n", "<leader>hr", gitsigns.reset_hunk)
+				map("v", "<leader>hs", function()
+					gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
 				end)
+				map("v", "<leader>hr", function()
+					gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end)
+				map("n", "<leader>hS", gitsigns.stage_buffer)
+				map("n", "<leader>hu", gitsigns.undo_stage_hunk)
+				map("n", "<leader>hR", gitsigns.reset_buffer)
+				map("n", "<leader>hp", gitsigns.preview_hunk)
+				map("n", "<leader>hb", function()
+					gitsigns.blame_line({ full = true })
+				end)
+				map("n", "<leader>tb", gitsigns.toggle_current_line_blame)
+				map("n", "<leader>hd", gitsigns.diffthis)
+				map("n", "<leader>hD", function()
+					gitsigns.diffthis("~")
+				end)
+				map("n", "<leader>td", gitsigns.toggle_deleted)
+
+				-- Text object
+				map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
 			end,
 		})
 	end,
