e7b89cdd15834c7a7900d11f6f7723de04d9d893
H4ckint0sh <alijan.adeli@icloud.com>
2024-10-24 21:56:37 +0200

    clean unused keymapings

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -1,15 +1,9 @@
 local keymap = vim.keymap
 
-keymap.set("v", "<S-PageDown>", ":m '>+1<CR>gv=gv") -- Move Line Down in Visual Mode
-keymap.set("v", "<S-PageUp>", ":m '<-2<CR>gv=gv") -- Move Line Up in Visual Mode
-keymap.set("n", "<S-PageDown>", ":m .+1<CR>==") -- Move Line Down in Normal Mode
-keymap.set("n", "<S-PageUp>", ":m .-2<CR>==") -- Move Line Up in Normal Mode
-
 -- Terminal
 -- Define terminal mode mapping in Lua
 vim.keymap.set("t", "<S-esc>", "<C-\\><C-n>")
 
--- init.lua
 -- Move line up
 keymap.set("n", "<A-u>", ":m .-2<cr>==")
 keymap.set("i", "<A-u>", "<esc>:m .-2<cr>==gi")
@@ -24,10 +18,6 @@ keymap.set("v", "<A-d>", ":m '>+1<CR>gv=gv")
 keymap.set("n", "<leader>sa", "ggVG")
 
 -- Keep stuff centred while moving around
-keymap.set("n", "*", "*zzzv")
-keymap.set("n", "#", "#zzzv")
-keymap.set("n", ",", ",zzzv")
-keymap.set("n", ";", ";zzzv")
 keymap.set("n", "n", "nzzzv")
 keymap.set("n", "N", "Nzzzv")
 
@@ -42,7 +32,6 @@ keymap.set("n", "<leader>p", '"_dP')
 keymap.set("v", "<", "<gv")
 keymap.set("v", ">", ">gv")
 
--- init.lua
 -- Duplicate line up
 keymap.set("n", "<A-S-u>", "yypk")
 keymap.set("v", "<A-S-u>", "yP`[V`]")
@@ -80,34 +69,11 @@ keymap.set("v", "<leader><C-r>", ":s/\\%V") -- Search only in visual selection u
 keymap.set("v", "<C-r>", '"hy:%s/\\v<C-r>h//g<left><left>', { silent = false }) -- change selection
 
 -- General keymaps
-keymap.set("i", "jk", "<ESC>") -- exit insert mode with jk
-keymap.set("i", "ii", "<ESC>") -- exit insert mode with ii
 keymap.set("n", "<leader>%", "ggVG") -- exit insert mode with ii
 keymap.set("n", "<leader>wq", ":wq<CR>") -- save and quit
 keymap.set("n", "<leader>qq", ":q!<CR>") -- quit without saving
 keymap.set("n", "<leader>ww", ":w<CR>") -- save
 keymap.set("n", "gx", ":!open <c-r><c-a><CR>") -- open URL under cursor
-
--- Split window management
-keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width
-keymap.set("n", "<leader>sx", ":close<CR>") -- close split window
-keymap.set("n", "<leader>sj", "<C-w>-") -- make split window height shorter
-keymap.set("n", "<leader>sk", "<C-w>+") -- make split windows height taller
-keymap.set("n", "<leader>sl", "<C-w>>5") -- make split windows width bigger
-keymap.set("n", "<leader>sh", "<C-w><5") -- make split windows width smaller
-
--- Tab management
-keymap.set("n", "<leader>to", ":tabnew<CR>") -- open a new tab
-keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close a tab
-keymap.set("n", "<leader>tn", ":tabn<CR>") -- next tab
-keymap.set("n", "<leader>tp", ":tabp<CR>") -- previous tab
-
--- Diff keymaps
-keymap.set("n", "<leader>cc", ":diffput<CR>") -- put diff from current to other during diff
-keymap.set("n", "<leader>cj", ":diffget 1<CR>") -- get diff from left (local) during merge
-keymap.set("n", "<leader>ck", ":diffget 3<CR>") -- get diff from right (remote) during merge
 
 -- Quickfix keymaps
 keymap.set("n", "<leader>qo", ":copen<CR>") -- open quickfix list
@@ -117,16 +83,8 @@ keymap.set("n", "<leader>qp", ":cprev<CR>") -- jump to prev quickfix list item
 keymap.set("n", "<leader>ql", ":clast<CR>") -- jump to last quickfix list item
 keymap.set("n", "<leader>qc", ":cclose<CR>") -- close quickfix list
 
--- Vim-maximizer
-keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle maximize tab
-
 -- got to previuos active tab
 keymap.set("n", "<leader>#", ":b#<CR>")
-
--- Nvim-tree
-keymap.set("n", "<leader>ee", ":NvimTreeToggle<CR>") -- toggle file explorer
-keymap.set("n", "<leader>er", ":NvimTreeFocus<CR>") -- toggle focus to file explorer
-keymap.set("n", "<leader>ef", ":NvimTreeFindFile<CR>") -- find file in file explorer
 
 -- Git-blame
 keymap.set("n", "<leader>gb", ":GitBlameToggle<CR>") -- toggle git blame
