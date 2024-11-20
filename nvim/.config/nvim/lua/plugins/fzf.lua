eb7a18170bf8b59ff166f3c0bb015df5360e4a69
H4ckint0sh <alijan.adeli@icloud.com>
2024-11-20 23:51:43 +0100

    base fzf on telescope

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -3,33 +3,9 @@ return {
 	-- optional for icon support
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		local action_ok, actions = pcall(require, "fzf-lua.actions")
-		local fzf_ok, fzf = pcall(require, "fzf-lua")
-
-		if not fzf_ok or not action_ok then
-			return
-		end
-
-		fzf.setup({
-			winopts = {
-				backdrop = 100,
-				preview = {
-					-- layout = "vertical" or "horizontal"
-					layout = "flex",
-				},
-			},
-			keymap = {
-				fzf = {
-					["ctrl-q"] = "select-all+accept",
-					["ctrl-u"] = "half-page-up",
-					["ctrl-d"] = "half-page-down",
-					["ctrl-x"] = "jump",
-				},
-				builtin = {
-					["ctrl-f"] = "preview-page-down",
-					["ctrl-b"] = "preview-page-up",
-				},
-			},
+		require("fzf-lua").setup({
+			{ "telescope" },
+			fzf_opts = { ["--layout"] = "reverse", ["--marker"] = "+" },
 			grep = {
 				rg_opts = table.concat({
 					"--column",
@@ -45,15 +21,6 @@ return {
 					"--hidden",
 					"-e",
 				}, " "),
-			},
-			actions = {
-				files = {
-					["default"] = actions.file_edit,
-					["ctrl-h"] = actions.file_split,
-					["ctrl-v"] = actions.file_vsplit,
-					["alt-q"] = actions.file_sel_to_qf,
-					["alt-l"] = actions.file_sel_to_ll,
-				},
 			},
 		})
 	end,
