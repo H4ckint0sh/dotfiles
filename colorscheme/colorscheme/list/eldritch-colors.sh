39d851e35b4f708ca7939880de78025ae1afdedf
H4ckint0sh <alijan.adeli@icloud.com>
2024-12-11 16:20:13 +0100

    initial commit

---
+++ colorscheme/colorscheme/list/eldritch-colors.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# Filename: ~/github/dotfiles-latest/colorscheme/list/eldritch-colors.sh
+# ~/github/dotfiles-latest/colorscheme/list/eldritch-colors.sh
+
+# Lighter markdown headings
+# 4 colors to the right for these ligher headings
+# https://www.color-hex.com/color/987afb
+#
+# Given that color A (#987afb) becomes color B (#5b4996) when darkened 4 steps
+# to the right, apply the same darkening ratio/pattern to calculate what color
+# C (#37f499) becomes when darkened 4 steps to the right.
+#
+# Markdown heading 1 - color04
+linkarzu_color18="#625491"
+# Markdown heading 2 - color02
+linkarzu_color19="#21925b"
+# Markdown heading 3 - color03
+linkarzu_color20="#027d95"
+# Markdown heading 4 - color01
+linkarzu_color21="#913c6d"
+# Markdown heading 5 - color05
+linkarzu_color22="#909748"
+# Markdown heading 6 - color08
+linkarzu_color23="#904146"
+
+linkarzu_color04="#a48cf2"
+linkarzu_color02="#37f499"
+linkarzu_color03="#04d1f9"
+linkarzu_color01="#f265b5"
+linkarzu_color05="#f1fc79"
+linkarzu_color08="#f16c75"
+linkarzu_color06="#f7c67f"
+# Markdown codeblock, lualine color across
+linkarzu_color07="#314154"
+# Comments
+linkarzu_color09="#a5afc2"
+# Terminal and neovim background
+linkarzu_color10="#212337"
+# Underline spellbad
+linkarzu_color11="#f16c75"
+# Underline spellcap
+linkarzu_color12="#f1fc79"
+# line across cursor
+linkarzu_color13="#314154"
+# Cursor and tmux windows text
+linkarzu_color14="#ebfafa"
+# Tmux inactive windows (7 colors to the right)
+linkarzu_color15="#013e4a"
+# Selected text
+linkarzu_color16="#d99ffd"
+# Lualine across
+linkarzu_color17="#282b43"
+# Cursor color
+linkarzu_color24="#F712FF"
+# Background of inactive tmux pane
+linkarzu_color25=#232e3b
