0fdbf6635240eb0fdd99321dcaa41bdeb784b307
H4ckint0sh <alijan.adeli@icloud.com>
2024-12-12 08:56:05 +0100

    blink update, update plugins and change sketchybar colors to match tokyonight

--- sketchybar/.config/sketchybar/colors.lua
+++ sketchybar/.config/sketchybar/colors.lua
@@ -1,31 +1,29 @@
--- Dracula Colors
-
 return {
-	black = 0xff414868,
-	white = 0xffc0caf5,
+	black = 0xff737aa2, -- Background color
+	white = 0xffc0caf5, -- Foreground color
 	red = 0xfff7768e,
 	green = 0xff9ece6a,
-	blue = 0xff5199ba,
+	blue = 0xff7aa2f7,
 	yellow = 0xffe0af68,
 	orange = 0xffff9e64,
-	pink = 0xffFF80BF,
-	purple = 0xffbb9af7,
-	other_purple = 0xff9d7cd8,
+	pink = 0xffc678dd,
+	purple = 0xff9d7cd8,
+	other_purple = 0xff98c379,
 	cyan = 0xff7dcfff,
-	grey = 0xff7f8490,
-	dirty_white = 0xc8cad3f5,
-	dark_grey = 0xff2b2736,
+	grey = 0xff757984,
+	dirty_white = 0xffa9b1d6,
+	dark_grey = 0xff282a36,
 	transparent = 0x00000000,
 	bar = {
-		bg = 0xf822212C,
-		border = 0xff2c2e34,
+		bg = 0xff1a1b26,
+		border = 0xff3a3c42,
 	},
 	popup = {
-		bg = 0xd322212c,
-		border = 0xd322212c,
+		bg = 0xff282a36,
+		border = 0xff3a3c42,
 	},
-	bg1 = 0x331e1d27,
-	bg2 = 0xff302c45,
+	bg1 = 0xff282a36,
+	bg2 = 0xff3a3c42,
 
 	with_alpha = function(color, alpha)
 		if alpha > 1.0 or alpha < 0.0 then
