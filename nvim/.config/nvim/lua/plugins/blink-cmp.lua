c31c52b6108e662858b2dd08a1b62031de5cc7ea
H4ckint0sh <alijan.adeli@icloud.com>
2024-12-13 13:31:00 +0100

    enabls cmdline completions

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -32,9 +32,18 @@ return {
 			providers = {
 				dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
 			},
-			cmdline = {
-				enabled = true,
-			},
+			cmdline = function()
+				local type = vim.fn.getcmdtype()
+				-- Search forward and backward
+				if type == "/" or type == "?" then
+					return { "buffer" }
+				end
+				-- Commands
+				if type == ":" then
+					return { "cmdline" }
+				end
+				return {}
+			end,
 		},
 		signature = { enabled = true },
 		keymap = {
