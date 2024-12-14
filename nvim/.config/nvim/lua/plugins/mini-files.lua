13d3391b8f47a6094a5e1a40e8cce075dce4f70e
H4ckint0sh <alijan.adeli@icloud.com>
2024-12-14 10:09:34 +0100

    feat: add mini-files

--- nvim/.config/nvim/lua/plugins/mini-files.lua
+++ nvim/.config/nvim/lua/plugins/mini-files.lua
@@ -48,7 +48,7 @@ return {
 		opts.windows = vim.tbl_deep_extend("force", opts.windows or {}, {
 			preview = true,
 			width_focus = 30,
-			width_preview = 60,
+			width_preview = 80,
 		})
 
 		opts.options = vim.tbl_deep_extend("force", opts.options or {}, {
