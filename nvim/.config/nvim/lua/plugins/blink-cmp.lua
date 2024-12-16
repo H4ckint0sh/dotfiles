92804b45c66cee0561cde9c760590eb5f0a80609
H4ckint0sh <alijan.adeli@icloud.com>
2024-12-16 20:50:21 +0100

    fix: blink cmdline completion

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -32,19 +32,18 @@ return {
 			providers = {
 				dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
 			},
-			cmdline = {},
-			-- cmdline = function()
-			-- 	local type = vim.fn.getcmdtype()
-			-- 	-- Search forward and backward
-			-- 	if type == "/" or type == "?" then
-			-- 		return { "buffer" }
-			-- 	end
-			-- 	-- Commands
-			-- 	if type == ":" then
-			-- 		return { "cmdline" }
-			-- 	end
-			-- 	return {}
-			-- end,
+			cmdline = function()
+				local type = vim.fn.getcmdtype()
+				-- Search forward and backward
+				if type == "/" or type == "?" then
+					return { "buffer" }
+				end
+				-- Commands
+				if type == ":" then
+					return { "cmdline" }
+				end
+				return {}
+			end,
 		},
 		signature = {
 			enabled = true,
@@ -58,7 +57,7 @@ return {
 		keymap = {
 			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
 			["<C-e>"] = { "hide", "fallback" },
-			["<CR>"] = { "accept", "fallback" },
+			["<CR>"] = { "select_and_accept" },
 			["<Tab>"] = { "snippet_forward", "fallback" },
 			["<S-Tab>"] = { "snippet_backward", "fallback" },
 			["<Up>"] = { "select_prev", "fallback" },
@@ -67,6 +66,11 @@ return {
 			["<C-j>"] = { "select_next", "fallback" },
 			["<C-b>"] = { "scroll_documentation_up", "fallback" },
 			["<C-f>"] = { "scroll_documentation_down", "fallback" },
+			cmdline = {
+				preset = "super-tab",
+				["<C-k>"] = { "select_prev", "fallback" },
+				["<C-j>"] = { "select_next", "fallback" },
+			},
 		},
 	},
 }
