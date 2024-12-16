6e62253e74ef18b59fa22e319e81cd2082d4ed25
H4ckint0sh <alijan.adeli@icloud.com>
2024-12-16 12:06:28 +0100

    no space-indicator

--- sketchybar/.config/sketchybar/items/spaces.lua
+++ sketchybar/.config/sketchybar/items/spaces.lua
@@ -1,5 +1,4 @@
 local colors = require("colors")
-local icons = require("icons")
 local settings = require("settings")
 local app_icons = require("helpers.app_icons")
 
@@ -212,60 +211,3 @@ space_window_observer:subscribe(
 		updateAllWorkspaces()
 	end
 )
-
--- Indicator for swapping menus and spaces
-local spaces_indicator = sbar.add("item", {
-	padding_left = -3,
-	padding_right = 10,
-	icon = {
-		padding_left = 8,
-		padding_right = 9,
-		color = colors.grey,
-		string = icons.switch.off,
-	},
-	label = {
-		width = 0,
-		padding_left = 0,
-		padding_right = 8,
-		string = "Spaces",
-		color = colors.grey,
-	},
-	background = {
-		color = colors.with_alpha(colors.bg2, 0.0),
-		border_color = colors.with_alpha(colors.bg1, 0.0),
-	},
-})
-
-local function toggleSpacesIndicator(currently_on)
-	spaces_indicator:set({
-		icon = currently_on and icons.switch.off or icons.switch.on,
-	})
-end
-
-spaces_indicator:subscribe("swap_menus_and_spaces", function()
-	local currently_on = spaces_indicator:query().icon.value == icons.switch.on
-	toggleSpacesIndicator(currently_on)
-end)
-
-local function animateSpacesIndicator(entered)
-	sbar.animate("tanh", 30, function()
-		spaces_indicator:set({
-			background = {
-				color = { alpha = entered and 1.0 or 0.0 },
-				border_color = { alpha = entered and 0.5 or 0.0 },
-			},
-			icon = { color = colors.grey },
-			label = { width = entered and "dynamic" or 0 },
-		})
-	end)
-end
-
-spaces_indicator:subscribe("mouse.entered", function()
-	animateSpacesIndicator(true)
-end)
-spaces_indicator:subscribe("mouse.exited", function()
-	animateSpacesIndicator(false)
-end)
-spaces_indicator:subscribe("mouse.clicked", function()
-	sbar.trigger("swap_menus_and_spaces")
-end)
