55623fc8aaa5b7d04298498021fb524aaf11126b
H4ckint0sh <alijan.adeli@icloud.com>
2024-12-18 22:57:04 +0100

    fix: fix blink highjacking enter key

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -57,7 +57,7 @@ return {
 		keymap = {
 			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
 			["<C-e>"] = { "hide", "fallback" },
-			["<CR>"] = { "select_and_accept" },
+			["<CR>"] = { "accept", "fallback" },
 			["<Tab>"] = { "snippet_forward", "fallback" },
 			["<S-Tab>"] = { "snippet_backward", "fallback" },
 			["<Up>"] = { "select_prev", "fallback" },
