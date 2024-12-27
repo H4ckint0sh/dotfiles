404021381bd91d1deaa94338a95d0d927364de6e
H4ckint0sh <alijan.adeli@icloud.com>
2024-12-27 09:53:13 +0100

    fix: dont open long messages in split

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -20,7 +20,7 @@ return {
 		presets = {
 			bottom_search = false, -- use a classic bottom cmdline for search
 			command_palette = true, -- position the cmdline and popupmenu together
-			long_message_to_split = true, -- long messages will be sent to a split
+			long_message_to_split = false, -- long messages will be sent to a split
 			inc_rename = false, -- enables an input dialog for inc-rename.nvim
 			lsp_doc_border = true, -- add a border to hover docs and signature help
 		},
