9345bb7aa3633a38c6c53f34327c6a48b715844a
H4ckint0sh <alijan.adeli@icloud.com>
2025-01-12 23:38:06 +0100

    feat: remove unused scripts and fix tmux cheat

--- bin/bin/tmux-cht.sh
+++ bin/bin/tmux-cht.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-selected=`cat ~/.tmux-cht-languages ~/.tmux-cht-command | fzf`
+selected=`cat ~/.config/tmux/.tmux-cht-languages ~/.config/tmux/.tmux-cht-command | fzf`
 if [[ -z $selected ]]; then
     exit 0
 fi
 
 read -p "Enter Query: " query
 
-if grep -qs "$selected" ~/.tmux-cht-languages; then
+if grep -qs "$selected" ~/.config/tmux/.tmux-cht-languages; then
     query=`echo $query | tr ' ' '+'`
     tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
 else
