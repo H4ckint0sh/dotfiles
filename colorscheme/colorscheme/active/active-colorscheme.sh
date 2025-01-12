fad39e27c412af3413467c07e9f8071a79eb85e5
H4ckint0sh <alijan.adeli@icloud.com>
2025-01-12 23:28:31 +0100

    fix sketchybar

--- colorscheme/colorscheme/active/active-colorscheme.sh
+++ colorscheme/colorscheme/active/active-colorscheme.sh
@@ -1,72 +1,62 @@
 #!/usr/bin/env bash
 
-# Filename: ~/github/dotfiles-latest/colorscheme/list/catppuccin-macchiato.sh
-# ~/github/dotfiles-latest/colorscheme/list/catppuccin-macchiato.sh
+# Filename: ~/github/dotfiles-latest/colorscheme/list/h4ckint0sh-colors.sh
+# ~/github/dotfiles-latest/colorscheme/list/h4ckint0sh-colors.sh
+
+# These files have to be executable
 
 # Lighter markdown headings
 # 4 colors to the right for these ligher headings
 # https://www.color-hex.com/color/987afb
 #
-# Given that color A (#987afb) becomes color B (#5b4996) when darkened 4 steps
-# to the right, apply the same darkening ratio/pattern to calculate what color
-# C (#37f499) becomes when darkened 4 steps to the right.
+# Given that color A (#987afb) becomes color B (#5b4996) when darkened 4 steps to the right, apply the same darkening ratio/pattern to calculate what color C (#37f499) becomes when darkened 4 steps to the right.
 #
 # Markdown heading 1 - color04
-h4ckint0sh_color18="#765f93"
+h4ckint0sh_color18="#855913"
 # Markdown heading 2 - color02
-h4ckint0sh_color19="#536892"
+h4ckint0sh_color19="#947e11"
 # Markdown heading 3 - color03
-h4ckint0sh_color20="#648359"
+h4ckint0sh_color20="#968f6f"
 # Markdown heading 4 - color01
-h4ckint0sh_color21="#8e5c60"
+h4ckint0sh_color21="#956a39"
 # Markdown heading 5 - color05
-h4ckint0sh_color22="#6d7195"
+h4ckint0sh_color22="#8b9494"
 # Markdown heading 6 - color08
-h4ckint0sh_color23="#93654c"
+h4ckint0sh_color23="#4c1205"
 
-# Magenta 
-h4ckint0sh_color04="#bb9af7"
-# Blue
-h4ckint0sh_color02="#7aa2f7"
-# Green
-h4ckint0sh_color03="#9ece6a"
-# Purple 
-h4ckint0sh_color01="#9d7cd8"
-# Blue1
-h4ckint0sh_color05="#3d59a1"
-# Magenta2
-h4ckint0sh_color08="#ff007c"
-# Blue1
-h4ckint0sh_color06="#2ac3de"
-# fg
-# Markdown codeblock, lualine color across
-h4ckint0sh_color07="#c0caf5"
-# Subtext0
+h4ckint0sh_color04="#e19720"
+h4ckint0sh_color02="#fad61d"
+h4ckint0sh_color03="#fdf2bb"
+h4ckint0sh_color01="#fbe260"
+h4ckint0sh_color05="#ebfafa"
+h4ckint0sh_color08="#811e09"
+h4ckint0sh_color06="#f62d14"
+# Markdown codeblock, 2 to the right of background
+# https://www.colorhexa.com
+h4ckint0sh_color07="#6d5d0c"
 # Comments
-h4ckint0sh_color09="#565f89"
-# Base
+h4ckint0sh_color09="#a5afc2"
 # Terminal and neovim background
-h4ckint0sh_color10="#1a1b26"
-# Red
+h4ckint0sh_color10="#4a3f08"
 # Underline spellbad
-h4ckint0sh_color11="#f7768e"
-# Yellow
+h4ckint0sh_color11="#f62d14"
 # Underline spellcap
-h4ckint0sh_color12="#e0af68"
-# Surface2
-# line across cursor
-h4ckint0sh_color13="#16161e"
-# Text
+h4ckint0sh_color12="#fad61d"
+# line across cursor, 5 to the right of background
+# https://www.colorhexa.com
+h4ckint0sh_color13="#a38a12"
 # Cursor and tmux windows text
-h4ckint0sh_color14="#c0caf5"
-# Surface1
-# Tmux inactive windows (7 colors to the right)
-h4ckint0sh_color15="#565f89"
-# Flamingo
+h4ckint0sh_color14="#ebfafa"
+# Tmux inactive windows, 7 colors to the right of background
+# https://www.colorhexa.com
+h4ckint0sh_color15="#4b4838"
 # Selected text
-h4ckint0sh_color16="#292e42"
-# Lualine across
-h4ckint0sh_color17="#a9b1d6"
+h4ckint0sh_color16="#e9b3fd"
+# Lualine across, 1 color to the right of background
+# https://www.colorhexa.com/0d1116
+h4ckint0sh_color17="#3b3206"
 # Cursor color
-h4ckint0sh_color24="#c0caf5"
-
+h4ckint0sh_color24="#F712FF"
+# Background of inactive, 3 to the right of background
+# https://www.colorhexa.come tmux pane
+h4ckint0sh_color25="#7f6c0e"
