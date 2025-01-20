6420defaee19b883dc775d7e737445a7469ad08b
H4ckint0sh <alijan.adeli@icloud.com>
2025-01-20 10:32:17 +0100

    feat: add neogit dependancies

--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -1,5 +1,9 @@
 return {
 	"NeogitOrg/neogit",
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- required
+		"sindrets/diffview.nvim",
+	},
 	keys = {
 		{ "<leader>n", ":Neogit<CR>", desc = "Neogit" },
 	},
