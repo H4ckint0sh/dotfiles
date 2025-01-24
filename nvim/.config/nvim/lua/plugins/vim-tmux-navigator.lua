95be02082f4772822059de13edd90ea27d0cc357
H4ckint0sh <alijan.adeli@icloud.com>
2025-01-24 12:08:05 +0100

    feat: command+w should close either the nvim buffer or the tmux pane

--- nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,12 +1,20 @@
--- Navigate nvim and tmux windows/panels with vim bindings
 return {
-  -- https://github.com/christoomey/vim-tmux-navigator
-  'christoomey/vim-tmux-navigator',
-  -- Only load this plugin if tmux is being used
-  event = function()
-    if vim.fn.exists("$TMUX") == 1 then
-      return "VeryLazy"
-    end
-  end,
+	"joshmedeski/vim-tmux-navigator",
+	cmd = {
+		"TmuxNavigateLeft",
+		"TmuxNavigateDown",
+		"TmuxNavigateUp",
+		"TmuxNavigateRight",
+		"TmuxNavigatePrevious",
+		"TmuxNavigatorProcessList",
+		"TmuxNavigateClose",
+	},
+	keys = {
+		{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+		{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+		{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+		{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+		{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		{ "<c-q>", "<cmd><C-U>TmuxNavigateClose<cr>" },
+	},
 }
-
