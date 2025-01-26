051e181cfa1bfa0e1651a7f6316b4aa47c1e62e3
H4ckint0sh <alijan.adeli@icloud.com>
2025-01-26 16:02:03 +0100

    feat: none functional nord theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,69 +1,32 @@
--- tokyonight Theme
 return {
-	-- https://github.com/tokyonight/nvim
-	"folke/tokyonight.nvim",
-	lazy = false,
+	"gbprod/nord.nvim",
 	priority = 1000,
-	opts = {
-		style = "night", -- The theme comes in three styles, `storm`, a darker variant `night` and
-		transparent = true, -- Enable this to disable setting the background color.
-		terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim.
-		dim_inactive = false, -- dim inactive windows
-		styles = {
-			-- Style to be applied to different syntax groups
-			-- Value is any valid attr-list value for `:help nvim_set_hl`
-			comments = { italic = true },
-			keywords = { italic = true },
-			conditionals = { italic = true },
-			functions = {},
-			variables = {},
-			-- Background styles. Can be "dark", "transparent" or "normal"
-			sidebars = "normal", -- style for sidebars, see below
-			floats = "normal", -- style for floating windows
-		},
-		on_highlights = function(H, C)
-			-- Native
-			H["@keyword.import"] = { fg = C.purple, italic = true }
-			H["@tag.tsx"] = { fg = C.blue2 }
-			H["@tag.builtin.tsx"] = { fg = C.red }
 
-			H.CursorLine = { bg = C.bg_highlight }
-			H.CursorLineNr = { fg = C.orange }
-			H.LspInlayHint = { bg = "NONE", fg = C.comment }
-			H.GitSignsCurrentLineBlame = { fg = C.comment }
-			H.DiagnosticVirtualTextError = { bg = "NONE", fg = C.red }
-			H.DiagnosticVirtualTextHint = { bg = "NONE", fg = C.teal }
-			H.DiagnosticVirtualTextInfo = { bg = "NONE", fg = C.green }
-			H.DiagnosticVirtualTextWarn = { bg = "NONE", fg = C.yellow }
-			H.WinSeparator = { fg = C.comment, bg = "NONE" }
-			H.TroubleNormal = { bg = "NONE", fg = C.fg }
+	config = function()
+		require("nord").setup({
+			styles = {
+				-- Style to be applied to different syntax groups
+				-- Value is any valid attr-list value for `:help nvim_set_hl`
+				comments = { italic = true },
+				keywords = { italic = true },
+				constants = { italic = true },
+				functions = {},
+				variables = {},
 
-			-- Neogit
-			H.NeogitSectionHeader = { fg = C.magenta }
+				-- To customize lualine/bufferline
+				bufferline = {
+					current = {},
+					modified = { italic = true },
+				},
+			},
+			on_highlights = function(highlights, colors)
+				-- Native
+				highlights["@tag.builtin.tsx"] = { fg = colors.red }
 
-			-- NvimTree
-			H.NvimTreeWinSeparator = { fg = C.comment, bg = C.bg_dark }
-
-			-- Fzf
-			H.FzfLuaBorder = { fg = C.comment }
-			H.FzfLuaCursorLine = { bg = C.bg_dark }
-			H.FzfLuaScrollFloatEmpty = { bg = C.fg }
-			H.FzfLuaScrollFloatEmpty = { bg = C.fg }
-			H.FzfLuaScrollFloatFull = { bg = C.fg }
-			H.FzfLuaFzfScrollbar = { fg = C.comment }
-			H.FzfLuaFzfBorder = { fg = C.comment }
-			H.FzfLuaPreviewTitle = { fg = C.fg }
-			H.FzfLuaCursorLine = { bg = C.bg_highlight }
-
-			-- Float
-			H.FloatBorder = { fg = C.comment, bg = "NONE" }
-
-			-- Snacks
-			H.SnacksDashboardHeader = { fg = C.green }
-		end,
-	},
-	config = function(_, opts)
-		require("tokyonight").setup(opts) -- Replace this with your favorite colorscheme
-		vim.cmd("colorscheme tokyonight") -- Replace this with your favorite colorscheme
+				-- Snacks
+				highlights.SnacksDashboardHeader = { fg = colors.green }
+			end,
+		})
+		vim.cmd.colorscheme("nord")
 	end,
 }
--- sketchybar/.config/sketchybar/colors.lua
+++ sketchybar/.config/sketchybar/colors.lua
@@ -1,29 +1,29 @@
 return {
-	black = 0xff1a1b26, -- Background color
-	white = 0xffc0caf5, -- Foreground color
-	red = 0xfff7768e,
-	green = 0xff9ece6a,
-	blue = 0xff7aa2f7,
-	yellow = 0xffe0af68,
-	orange = 0xffff9e64,
-	pink = 0xffc678dd,
-	purple = 0xff9d7cd8,
-	other_purple = 0xff98c379,
-	cyan = 0xff7dcfff,
-	grey = 0xff757984,
+	black = 0xff2E3440, -- Background color
+	white = 0xffD8DEE9, -- Foreground color
+	red = 0xffBF616A,
+	green = 0xffA3BE8C,
+	blue = 0xff5E81AC,
+	yellow = 0xffEBCB8B,
+	orange = 0xffD08770,
+	pink = 0xffB48EAD,
+	purple = 0xffB48EAD,
+	other_purple = 0xffB48EAD,
+	cyan = 0xff88C0D0,
+	grey = 0xffE5E9F0,
 	dirty_white = 0xffa9b1d6,
 	dark_grey = 0xff282a36,
 	transparent = 0x00000000,
 	bar = {
-		bg = 0xff0C0E14,
+		bg = 0xff2E3440,
 		border = 0xff3a3c42,
 	},
 	popup = {
-		bg = 0xcf1a1b26,
+		bg = 0xcf2E3440,
 		border = 0xff3a3c42,
 	},
-	bg1 = 0xff1a1b26,
-	bg2 = 0xff16161e,
+	bg1 = 0xff2E3440,
+	bg2 = 0xff2E3440,
 
 	with_alpha = function(color, alpha)
 		if alpha > 1.0 or alpha < 0.0 then
