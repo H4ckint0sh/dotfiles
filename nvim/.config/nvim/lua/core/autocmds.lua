cff236f8284b88af6408b1bc47de040310cdc80e
H4ckint0sh <alijan.adeli@icloud.com>
2025-01-29 17:35:15 +0100

    fix: yank highlight

--- nvim/.config/nvim/lua/core/autocmds.lua
+++ nvim/.config/nvim/lua/core/autocmds.lua
@@ -1,9 +1,3 @@
--- Highlight on yank
-vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 100 })
-	end,
-})
 -- Disable diagnostics in node_modules (0 is current buffer only)
 vim.api.nvim_create_autocmd(
 	{ "BufRead", "BufNewFile" },
@@ -23,7 +17,7 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 vim.api.nvim_create_autocmd("TextYankPost", {
 	group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
 	callback = function()
-		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 100 })
+		vim.highlight.on_yank({ higroup = "TabLineSel", timeout = 150 })
 	end,
 })
 
