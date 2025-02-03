e73b18eec1ac422c07c6e7884eb368f1def5106d
H4ckint0sh <alijan.adeli@icloud.com>
2025-02-03 22:14:05 +0100

    feat: add flash ability to pickers and change some picker specific settings

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -9,72 +9,76 @@ return {
 			size = 100 * 1024, -- 100 KB
 		},
 		picker = {
-			files = {
-				hidden = true,
+			-- My ~/github/dotfiles-latest/neovim/lazyvim/lua/config/keymaps.lua
+			-- file was always showing at the top, I needed a way to decrease its
+			-- score, in frecency you could use :FrecencyDelete to delete a file
+			-- from the database, here you can decrease it's score
+			transform = function(item)
+				if not item.file then
+					return item
+				end
+				-- Demote the "lazyvim" keymaps file:
+				if item.file:match("lazyvim/lua/config/keymaps%.lua") then
+					item.score_add = (item.score_add or 0) - 30
+				end
+				-- Boost the "neobean" keymaps file:
+				-- if item.file:match("neobean/lua/config/keymaps%.lua") then
+				--   item.score_add = (item.score_add or 0) + 100
+				-- end
+				return item
+			end,
+			-- In case you want to make sure that the score manipulation above works
+			-- or if you want to check the score of each file
+			debug = {
+				scores = true, -- show scores in the list
 			},
+			-- I like the "ivy" layout, so I set it as the default globaly, you can
+			-- still override it in different keymaps
 			layout = {
-				preset = function()
-					return vim.o.columns >= 120 and "default" or "vertical"
-				end,
+				preset = "ivy",
+				-- When reaching the bottom of the results in the picker, I don't want
+				-- it to cycle and go back to the top
+				cycle = false,
 			},
 			layouts = {
-				telescope = {
-					reverse = true,
+				-- I wanted to modify the ivy layout height and preview pane width,
+				-- this is the only way I was able to do it
+				-- NOTE: I don't think this is the right way as I'm declaring all the
+				-- other values below, if you know a better way, let me know
+				--
+				-- Then call this layout in the keymaps above
+				-- got example from here
+				-- https://github.com/folke/snacks.nvim/discussions/468
+				ivy = {
 					layout = {
-						box = "horizontal",
+						box = "vertical",
 						backdrop = false,
-						width = 0.9,
-						height = 0.9,
-						border = "none",
+						row = -1,
+						width = 0,
+						height = 0.5,
+						border = "top",
+						title = " {title} {live} {flags}",
+						title_pos = "left",
+						{ win = "input", height = 1, border = "bottom" },
 						{
-							box = "vertical",
-							border = "rounded",
+							box = "horizontal",
 							{ win = "list", border = "none" },
-							{
-								win = "input",
-								height = 1,
-								border = "top",
-								title = "{title} {live} {flags}",
-								title_pos = "center",
-							},
-						},
-						{
-							win = "preview",
-							title = "{preview:Preview}",
-							width = 0.55,
-							border = "rounded",
-							title_pos = "center",
+							{ win = "preview", title = "{preview}", width = 0.5, border = "left" },
 						},
 					},
 				},
-				default = {
-					layout = {
-						box = "horizontal",
-						backdrop = false,
-						width = 0.9,
-						min_width = 120,
-						height = 0.8,
-						{
-							box = "vertical",
-							border = "rounded",
-							title = "{title} {live} {flags}",
-							{ win = "input", height = 1, border = "bottom" },
-							{ win = "list", border = "none" },
-						},
-						{ win = "preview", title = "{preview}", border = "rounded", width = 0.6 },
-					},
-				},
-				select = {
-					preview = false,
+				-- I wanted to modify the layout width
+				--
+				vertical = {
 					layout = {
 						backdrop = false,
-						width = 0.3,
+						width = 0.8,
 						min_width = 80,
-						height = 0.4,
-						min_height = 10,
+						height = 0.8,
+						min_height = 30,
 						box = "vertical",
 						border = "rounded",
-						title = " Select ",
+						title = "{title} {live} {flags}",
 						title_pos = "center",
 						{ win = "input", height = 1, border = "bottom" },
 						{ win = "list", border = "none" },
@@ -82,11 +86,42 @@ return {
 					},
 				},
 			},
+			matcher = {
+				frecency = true,
+			},
+			actions = {
+				flash = function(picker)
+					require("flash").jump({
+						pattern = "^",
+						label = { after = { 0, 0 } },
+						search = {
+							mode = "search",
+							exclude = {
+								function(win)
+									return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "snacks_picker_list"
+								end,
+							},
+						},
+						action = function(match)
+							local idx = picker.list:row2idx(match.pos[1])
+							picker.list:_move(idx, true, true)
+						end,
+					})
+				end,
+			},
 			win = {
 				input = {
 					keys = {
+						-- to close the picker on ESC instead of going to normal mode,
+						-- add the following keymap to your config
 						["<Esc>"] = { "close", mode = { "n", "i" } },
-						["<a-.>"] = { "toggle_hidden", mode = { "i", "n" } },
+						-- I'm used to scrolling like this in LazyGit
+						["J"] = { "preview_scroll_down", mode = { "i", "n" } },
+						["K"] = { "preview_scroll_up", mode = { "i", "n" } },
+						["H"] = { "preview_scroll_left", mode = { "i", "n" } },
+						["L"] = { "preview_scroll_right", mode = { "i", "n" } },
+						["<a-s>"] = { "flash", mode = { "n", "i" } },
+						["s"] = { "flash" },
 					},
 				},
 			},
@@ -227,12 +262,64 @@ return {
 		{ "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference" },
 		{ "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference" },
 		{ "<leader>z",  function() Snacks.zen() end, desc = "ZEN" },
-		{ "<leader>,", function() Snacks.picker.buffers() end, desc = "Buffers" },
+		{ "<leader>,", function() Snacks.picker.buffers(
+			{
+            -- I always want my buffers picker to start in normal mode
+            on_show = function()
+              vim.cmd.stopinsert()
+            end,
+            finder = "buffers",
+            format = "buffer",
+            hidden = false,
+            unloaded = true,
+            current = true,
+            sort_lastused = true,
+            win = {
+              input = {
+                keys = {
+                  ["d"] = "bufdelete",
+                },
+              },
+              list = { keys = { ["d"] = "bufdelete" } },
+            },
+            -- In case you want to override the layout for this keymap
+            -- layout = "ivy",
+          }
+		) end, desc = "Buffers" },
 		{ "<leader>t", function() Snacks.picker.grep({ layout = { preset = "ivy" } , hidden = true }) end, desc = "Grep" },
 		{ "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
-		{ "<leader>f", function() Snacks.picker.files({hidden = true}) end, desc = "Find Files" },
+		{ "<leader>f", function() Snacks.picker.files({
+            finder = "files",
+            format = "file",
+			hidden = true,
+            show_empty = true,
+            supports_live = true,
+            -- In case you want to override the layout for this keymap
+            -- layout = "vscode",
+          }) end, desc = "Find Files" },
 		-- find
-		{ "<leader>b", function() Snacks.picker.buffers({ layout = { preset = "select" } }) end, desc = "Buffers" },
+		{ "<leader>b", function() Snacks.picker.buffers({
+            -- I always want my buffers picker to start in normal mode
+            on_show = function()
+              vim.cmd.stopinsert()
+            end,
+            finder = "buffers",
+            format = "buffer",
+            hidden = false,
+            unloaded = true,
+            current = true,
+            sort_lastused = true,
+            win = {
+              input = {
+                keys = {
+                  ["d"] = "bufdelete",
+                },
+              },
+              list = { keys = { ["d"] = "bufdelete" } },
+            },
+            -- In case you want to override the layout for this keymap
+            -- layout = "ivy",
+          }) end, desc = "Buffers" },
 		{ "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
 		{ "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
 		{ "<leader>fh", function() Snacks.picker.recent({layout = { preset = "select" } }) end, desc = "Recent" },
