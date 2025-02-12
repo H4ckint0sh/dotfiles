e199feb9a8cc7b1d3853d72ecf7be2ef552f078b
H4ckint0sh <alijan.adeli@icloud.com>
2025-02-12 10:08:44 +0100

    feat: disable auto pairs for snacks_picker_input

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -1,17 +1,10 @@
 return {
 	{
-		"altermo/ultimate-autopair.nvim",
-		event = { "InsertEnter", "CmdlineEnter" },
-		branch = "v0.6", --recommended as each new version will have breaking changes
-		opts = {
-			--Config goes here
-		},
-	},
-	{
 		"windwp/nvim-autopairs",
 		event = "InsertEnter",
 		config = true,
-		-- use opts = {} for passing setup options
-		-- this is equivalent to setup({}) function
+		opts = {
+			disable_filetype = { "snacks_picker_input" },
+		},
 	},
 }
