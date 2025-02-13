93d034c1d09713c71a3a1b81b44ac3b779c2466b
H4ckint0sh <alijan.adeli@icloud.com>
2025-02-13 17:53:39 +0100

    feat: slightly thinner status column (gitsigns)

--- nvim/.config/nvim/lua/plugins/nvim-ufo.lua
+++ nvim/.config/nvim/lua/plugins/nvim-ufo.lua
@@ -9,7 +9,10 @@ return {
 				require("statuscol").setup({
 					relculright = true,
 					segments = {
-						{ text = { "%s" }, click = "v:lua.ScSa" },
+						{
+							sign = { namespace = { "gitsigns" }, colwidth = 1, maxwidth = 1 },
+							click = "v:lua.ScSa",
+						},
 						{ text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
 						{ text = { builtin.foldfunc, " " }, click = "v:lua.ScFa" },
 					},
