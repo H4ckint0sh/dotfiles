20ea04f79d9b41198f133cbe96f50bf1895ea384
H4ckint0sh <alijan.adeli@icloud.com>
2025-02-18 18:57:04 +0100

    feat: add tmux support till tree-sitter

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -42,6 +42,7 @@ return {
 					"diff",
 					"git_rebase",
 					"toml",
+					"tmux",
 				}, -- one of "all", or a list of languages
 				sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
 				ignore_install = { "haskell" }, -- list of parsers to ignore installing
