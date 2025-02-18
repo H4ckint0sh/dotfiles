a1004ca84b4ae518c99e3badaa8f15a3d2fc4e96
H4ckint0sh <alijan.adeli@icloud.com>
2025-02-18 17:54:50 +0100

    feat: add toml support

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -41,6 +41,7 @@ return {
 					"sql",
 					"diff",
 					"git_rebase",
+					"toml",
 				}, -- one of "all", or a list of languages
 				sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
 				ignore_install = { "haskell" }, -- list of parsers to ignore installing
