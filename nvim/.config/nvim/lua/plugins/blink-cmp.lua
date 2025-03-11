9a58845522138fca01831ed5d781e2be5094d530
H4ckint0sh <alijan.adeli@icloud.com>
2025-03-11 21:26:28 +0100

    fix: cmmdline completions

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -49,11 +49,8 @@ return {
 			},
 		},
 		cmdline = {
-			keymap = {
-				preset = "super-tab",
-				["<C-k>"] = { "select_prev", "fallback" },
-				["<C-j>"] = { "select_next", "fallback" },
-			},
+			enabled = true,
+			keymap = { preset = "cmdline" },
 			sources = function()
 				local type = vim.fn.getcmdtype()
 				-- Search forward and backward
@@ -61,11 +58,29 @@ return {
 					return { "buffer" }
 				end
 				-- Commands
-				if type == ":" then
+				if type == ":" or type == "@" then
 					return { "cmdline" }
 				end
 				return {}
 			end,
+			completion = {
+				trigger = {
+					show_on_blocked_trigger_characters = {},
+					show_on_x_blocked_trigger_characters = {},
+				},
+				list = {
+					selection = {
+						-- When `true`, will automatically select the first item in the completion list
+						preselect = true,
+						-- When `true`, inserts the completion item automatically when selecting it
+						auto_insert = true,
+					},
+				},
+				-- Whether to automatically show the window when new completion items are available
+				menu = { auto_show = true },
+				-- Displays a preview of the selected item on the current line
+				ghost_text = { enabled = true },
+			},
 		},
 		signature = {
 			enabled = true,
