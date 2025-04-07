c4291c75a688aa6e3cfba76330373ca85d1a988b
H4ckint0sh <alijan.adeli@icloud.com>
2025-04-07 21:28:44 +0200

    feat: simplify

--- nvim/.config/nvim/lua/plugins/html-css.lua
+++ nvim/.config/nvim/lua/plugins/html-css.lua
@@ -2,7 +2,8 @@ return {
 	"Jezda1337/nvim-html-css",
 	dependencies = { "saghen/blink.cmp", "nvim-treesitter/nvim-treesitter" }, -- Use this if you're using blink.cmp
 	opts = {
-		enable_on = { -- Example file types
+		enable_on = {
+			"txt",
 			"html",
 			"htmldjango",
 			"tsx",
@@ -14,25 +15,11 @@ return {
 			"php",
 			"templ",
 			"astro",
-		},
-		handlers = {
-			definition = {
-				bind = "gd",
-			},
-			hover = {
-				bind = "K",
-				wrap = true,
-				border = "none",
-				position = "cursor",
-			},
+			"tmpl",
 		},
 		documentation = {
 			auto_show = true,
 		},
-		style_sheets = {
-			"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
-			"https://cdnjs.cloudflare.com/ajax/libs/bulma/1.0.3/css/bulma.min.css",
-			"./index.css", -- `./` refers to the current working directory.
-		},
+		style_sheets = {},
 	},
 }
