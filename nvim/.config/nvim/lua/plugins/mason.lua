34242a283da63f9316e86d778f297ed17a550b97
H4ckint0sh <alijan.adeli@icloud.com>
2025-05-07 12:35:15 +0200

    downgrade to v 1 mason and masonlsp-config

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,7 +1,15 @@
 return {
 	"williamboman/mason.nvim",
+	-- TODO: Upgrade to 2.0.0
+	-- Requires mason-lspconfig.nvim 2.0.0
+	version = "1.*",
 	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
+		{
+			"williamboman/mason-lspconfig.nvim",
+			-- TODO: Upgrade to 2.0.0
+			-- mappings/filetype.lua is removed in 2.0.0
+			version = "1.*",
+		},
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 	},
 	opts = {
