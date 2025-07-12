55addb44145a6fe416fc3b685310989f44ec03f3
H4ckint0sh <alijan.adeli@icloud.com>
2025-07-12 23:28:29 +0200

    fix: typo and comments

--- nvim/.config/nvim/lua/util/lualine.lua
+++ nvim/.config/nvim/lua/util/lualine.lua
@@ -132,35 +132,6 @@ M.lsp_formater_linter = {
 			buf_client_names[num_client_names] = client.name
 		end
 
-		-- Add linters for the current filetype (nvim-lint)
-		-- local lint_success, lint = pcall(require, "lint")
-		-- if lint_success then
-		-- 	for ft, ft_linters in pairs(lint.linters_by_ft) do
-		-- 		if ft == buf_ft then
-		-- 			if type(ft_linters) == "table" then
-		-- 				for _, linter in pairs(ft_linters) do
-		-- 					num_client_names = num_client_names + 1
-		-- 					buf_client_names[num_client_names] = linter
-		-- 				end
-		-- 			else
-		-- 				num_client_names = num_client_names + 1
-		-- 				buf_client_names[num_client_names] = ft_linters
-		-- 			end
-		-- 		end
-		-- 	end
-		-- end
-		--
-		-- -- Add formatters (conform.nvim)
-		-- local conform_success, conform = pcall(require, "conform")
-		-- if conform_success then
-		-- 	for _, formatter in pairs(conform.list_formatters_for_buffer(0)) do
-		-- 		if formatter then
-		-- 			num_client_names = num_client_names + 1
-		-- 			buf_client_names[num_client_names] = formatter
-		-- 		end
-		-- 	end
-		-- end
-
 		local client_names_str = table.concat(buf_client_names, ", ")
 		local language_servers = string.format("[%s]", client_names_str)
 
