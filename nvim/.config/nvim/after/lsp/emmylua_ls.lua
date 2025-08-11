977a7d92c2d9165230196d46c11d77311783facd
H4ckint0sh <alijan.adeli@icloud.com>
2025-08-11 16:20:19 +0200

    fix: vim global error

--- nvim/.config/nvim/after/lsp/emmylua_ls.lua
+++ nvim/.config/nvim/after/lsp/emmylua_ls.lua
@@ -2,5 +2,11 @@ return {
 	cmd = { "emmylua_ls" },
 	filetypes = { "lua" },
 	root_markers = { ".emmyrc.json", ".luarc.json", ".luarc.jsonc" },
-	settings = {},
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" }, -- tell the LS about the vim global
+			},
+		},
+	},
 }
