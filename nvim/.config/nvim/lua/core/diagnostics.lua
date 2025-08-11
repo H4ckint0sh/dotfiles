31b4ebe6b782ec7647bf03732b735b4f9b15a599
H4ckint0sh <alijan.adeli@icloud.com>
2025-08-11 14:26:25 +0200

    fix: ]d and [d are builtin now

--- nvim/.config/nvim/lua/core/diagnostics.lua
+++ nvim/.config/nvim/lua/core/diagnostics.lua
@@ -35,12 +35,6 @@ vim.diagnostic.config({
 	severity_sort = true,
 })
 
-vim.keymap.set("n", "[d", function()
-	vim.diagnostic.jump({ count = -1, float = false })
-end, { desc = "Prev diagnostic" })
-vim.keymap.set("n", "]d", function()
-	vim.diagnostic.jump({ count = 1, float = false })
-end, { desc = "Next diagnostic" })
 vim.keymap.set("n", "[e", function()
 	vim.diagnostic.jump({
 		count = -1,
