a41f49960a4562aefbb63734a70eddface27b044
H4ckint0sh <alijan.adeli@icloud.com>
2025-09-05 23:13:57 +0200

    feat: auto hlsearch

--- nvim/.config/nvim/lua/core/autocmds.lua
+++ nvim/.config/nvim/lua/core/autocmds.lua
@@ -114,3 +114,15 @@ vim.api.nvim_create_autocmd({ "TextChangedI", "InsertLeave", "TextYankPost" }, {
 		vim.diagnostic.setloclist({ open = false }) -- or vim.diagnostic.show()
 	end,
 })
+
+-- Auto nohlsearch
+vim.api.nvim_create_autocmd("CursorMoved", {
+	group = vim.api.nvim_create_augroup("auto-hlsearch", { clear = true }),
+	callback = function()
+		if vim.v.hlsearch == 1 and vim.fn.searchcount().exact_match == 0 then
+			vim.schedule(function()
+				vim.cmd.nohlsearch()
+			end)
+		end
+	end,
+})
