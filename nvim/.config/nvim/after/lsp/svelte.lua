3805f3804c69cd7a4c594346caddb4b627b1cf4a
H4ckint0sh <alijan.adeli@icloud.com>
2025-10-07 22:38:31 +0200

    feat: add svelte configuration

---
+++ nvim/.config/nvim/after/lsp/svelte.lua
@@ -0,0 +1,19 @@
+return {
+	settings = {
+		svelte = {
+			plugin = {
+				typescript = {
+					enable = true,
+					diagnostics = { enable = true },
+					hover = { enable = true },
+					documentSymbols = { enable = true },
+					completions = { enable = true },
+					findReferences = { enable = true },
+					definitions = { enable = true },
+					rename = { enable = true },
+					codeActions = { enable = true },
+				},
+			},
+		},
+	},
+}
